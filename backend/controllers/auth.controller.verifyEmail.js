2e431333b0599b38f6590d4a66d3c529c46c83b7
Pedro Ives <47249629+pedroives-public@users.noreply.github.com>
2024-10-21 15:37:01 -0300

    Implement verify email feature for user verification

---
+++ backend/controllers/auth.controller.verifyEmail.js
@@ -0,0 +1,39 @@
+import { User } from "../models/user.model.js";
+
+export const verifyEmail = async (req, res) => {
+  const { verificationToken } = req.body;
+  try {
+    const user = await User.findOne({
+      verificationToken: verificationToken,
+      verificationTokenExpiresAt: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid or expired verification token",
+      });
+    }
+
+    user.isVerified = true;
+    user.verificationToken = undefined;
+    user.verificationTokenExpiresAt = undefined;
+
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Email verified successfully",
+      user: {
+        name: user.name,
+        email: user.email,
+        isVerified: user.isVerified,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
