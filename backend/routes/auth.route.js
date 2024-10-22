9934fb27e5a7e2e5bad0c4e8f4fc1a75932ace4f
Pedro Ives <47249629+pedroives-public@users.noreply.github.com>
2024-10-22 17:35:24 -0300

    Update auth routes to support signout implementation

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 
+import { signout } from "../controllers/auth.controller.signout.js";
 import { signup } from "../controllers/auth.controller.signup.js";
 import { verifyEmail } from "../controllers/auth.controller.verifyEmail.js";
 
 const router = express.Router();
 
 router.post("/signin");
-router.post("/signout");
+router.post("/signout", signout);
 router.post("/signup", signup);
 
 router.post("/verify-email", verifyEmail);
