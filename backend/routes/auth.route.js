802cd9ae2f6835013215ebbdc9619af9be2089ca
Pedro Ives <47249629+pedroives-public@users.noreply.github.com>
2024-10-22 18:41:20 -0300

    Update auth routes to support signin implementation

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 
+import { signin } from "../controllers/auth.controller.signin.js";
 import { signout } from "../controllers/auth.controller.signout.js";
 import { signup } from "../controllers/auth.controller.signup.js";
 import { verifyEmail } from "../controllers/auth.controller.verifyEmail.js";
 
 const router = express.Router();
 
-router.post("/signin");
+router.post("/signin", signin);
 router.post("/signout", signout);
 router.post("/signup", signup);
 
