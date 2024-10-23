ddccac8a4d945b68dffea2e472521dec0db3389c
Pedro Ives <47249629+pedroives-public@users.noreply.github.com>
2024-10-23 19:54:57 -0300

    Implement forgotPassword feature to handle password reset requests

---
+++ backend/controllers/auth.controller.forgotPassword.js
@@ -0,0 +1,34 @@
+import bcryptjs from "bcryptjs";
+
+import { User } from "../models/user.model.js";
+import { generateResetPasswordToken } from "../utils/generateResetPasswordToken.js";
+import { sendResetPasswordEmail } from "../mailtrap/utils/sendResetPasswordEmail.js";
+
+export const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Generate reset token
+    const resetPasswordToken = generateResetPasswordToken();
+    const resetPasswordTokenExpiresAt = Date.now() + 1 * 60 * 60 * 1000;
+
+    // Save reset token to user
+    user.resetPasswordToken = resetPasswordToken;
+    user.resetPasswordTokenExpiresAt = resetPasswordTokenExpiresAt;
+    await user.save();
+
+    // Send reset password email
+    const resetPasswordUrl = `${req.protocol}://${req.get(
+      "host"
+    )}/reset-password/${resetPasswordToken}`;
+    await sendResetPasswordEmail(user.email, user.name, resetPasswordUrl);
+
+    res.status(200).json({ message: "Password reset link sent to email" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
