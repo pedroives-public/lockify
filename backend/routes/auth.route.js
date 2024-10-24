0a11ca7baf7ee10a65318b5e1e5f677ce18e8bab
Pedro Ives <47249629+pedroives-public@users.noreply.github.com>
2024-10-24 19:28:54 -0300

    Update auth routes to support verifyToken middleware and checkAuth controller

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -4,11 +4,15 @@ import { signin } from "../controllers/auth.controller.signin.js";
 import { signout } from "../controllers/auth.controller.signout.js";
 import { signup } from "../controllers/auth.controller.signup.js";
 
+import { checkAuth } from "../controllers/auth.controller.checkAuth.js";
 import { verifyEmail } from "../controllers/auth.controller.verifyEmail.js";
 import { forgotPassword } from "../controllers/auth.controller.forgotPassword.js";
 import { resetPassword } from "../controllers/auth.controller.resetPassword.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/check-auth", verifyToken, checkAuth);
 
 router.post("/signin", signin);
 router.post("/signout", signout);
